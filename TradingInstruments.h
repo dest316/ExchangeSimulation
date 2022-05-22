ba4dc700017dde8485b5bb7845b0eb8d3e0ea9c5
dest316 <90666585+dest316@users.noreply.github.com>
2022-05-22 20:45:11 +1000

    Update TradingInstruments.h

--- TradingInstruments.h
+++ TradingInstruments.h
@@ -4,32 +4,86 @@
 #include <string>
 using namespace std;
 
+class Glass
+{
+private:
+	vector<int> glassToBuy;
+	vector<int> glassToSell;
+	void Sort()
+	{	
+		for (int i = 1; i < glassToBuy.size(); i++)
+			for (int j = i; j > 0 && glassToBuy[j - 1] > glassToBuy[j]; j--)
+				swap(glassToBuy[j - 1], glassToBuy[j]);
+		for (int i = 1; i < glassToSell.size(); i++)
+			for (int j = i; j > 0 && glassToSell[j - 1] > glassToSell[j]; j--)
+				swap(glassToSell[j - 1], glassToSell[j]);
+	}
+public:
+	Glass() //потом можно будет добавить регулирование объема стакана, пока константа = 100 в обе стороны
+	{
+		for (size_t i = 0; i < 100; i++)
+		{
+			auto a = rand() % 100;
+			auto b = 100 + (rand() % 100);
+			glassToBuy.push_back(a);
+			glassToSell.push_back(b);
+			Sort();
+		}
+	}
+	vector<int>& GetGlassToBuy() { return glassToBuy; }
+	vector<int>& GetGlassToSell() { return glassToSell; }
+};
+
 class Order
 {
+private:
 	double price;
 	bool type;
-
+public:
+	Order() = default;
+	Order(double price, bool type)
+	{
+		this->price = price;
+		this->type = type;
+	}
+	double GetPrice() {return price; }
 };
 
 class TradingInstrument
 {
 protected:
-	string _name;
-	double _price;
-	int _orderByBuyCount;
-	int _orderBySellCount;
-	double _longLeverage;
-	double _shortLeverage;
-	vector<Order> _listOfOrders;
+	string name;
+	double priceToBuy;
+	double priceToSell;
+	double longLeverage;
+	double shortLeverage;
+	Glass* glass;
+
 
 public:
-	vector<Order> GetListOfOrders() { return _listOfOrders; }
-	double GetPrice() { return _price; }
-	void SetOrderBySellCount(int count) { _orderBySellCount -= count; }
-	double GetOrderBySellCount() { return _orderBySellCount; }
-	
-	
+	TradingInstrument(string name)
+	{
+		this->name = name;
+		glass = new Glass();
+		priceToBuy = glass->GetGlassToBuy()[glass->GetGlassToBuy().size() - 1];
+		priceToSell = glass->GetGlassToSell()[0];
+	}
+	TradingInstrument() = default;
+	double GetPriceToBuy() { return priceToBuy; }
+	double GetPriceToSell() { return priceToSell; }
+	double GetAveragePrice() { return (priceToBuy + priceToSell) / 2; }
+	string GetName() { return name; }
+	Glass& GetGlass() { return (*glass); }
+	bool IsBuyable() { return (!glass->GetGlassToSell().empty()); }
+	bool operator==(const TradingInstrument& other) { return this->name == other.name; }
+};
 
-	
+
+
+class Stock : public TradingInstrument
+{
+public:
+	Stock(string name) : TradingInstrument(name) {}
+	Stock() = default;
 };
 
